ceafecd209039aca2dcd5ad281f783be20798a32
ooobsidian <obsidian_lrx@aliyun.com>
2018-06-27 18:59:20 +0800

    style

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import echarts from 'echarts'
 
+Vue.prototype.$echarts = echarts
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Chart from 'components/chart.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      path:'/',
+      name:'root',
+      redirect:{name:'chart'}
+    },
+    {
+      path:'chart',
+      name:'chart',
+      component:Chart
+    }
+  ]
 })
